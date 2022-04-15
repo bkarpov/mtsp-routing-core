9ffc84fc3aee0b432217f610f84c733ecfaaab98
bkarpov <79852405770@ya.ru>
2022-04-15 19:09:38 +0300

    Добавлен тест времени выполнения с разным кол-вом параллельных процессов

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -1,6 +1,10 @@
 """Тесты отдельных функций, используемых при решении MTSP, и всего решения в целом"""
 
 
+import itertools
+import pytest
+from timeit import default_timer as timer
+
 from routing import solution as sl
 from routing import spatial_objects as sp
 
@@ -142,10 +146,55 @@ def test_solution() -> None:
     for edge in edges:
         graph.add_edge(edge)
 
-    results = list(sl.build_routes(list(points), clusters_amt, graph))
+    results = list(sl.build_routes(list(points), clusters_amt, graph))  # Маршрутизация с максимальной нагрузкой на CPU
 
     assert set(results[0][0]) == set(points[:6])  # Кластер с 1 фигурой
     assert set(results[0][1]) == set(edges[:6])  # Маршрут обхода совпадает с контуром
 
     assert set(results[1][0]) == set(points[6:])  # Кластер с 2 фигурой
     assert set(results[1][1]) == set(edges[14:20])  # Маршрут обхода совпадает с контуром
+
+
+@pytest.mark.parametrize("processes_num", [1, 2, 4])  # Маркировка теста для многократного выполнения
+def test_execution_time(processes_num: int) -> None:
+    """Тест времени выполнения маршрутизации
+
+    Чтобы тест работал корректно, количество логических процессоров должно быть не менее 4
+
+    Args:
+        processes_num: Количество доступных процессов
+    """
+
+    clusters_amt = 4
+
+    points = (
+        sp.Point(2, 1), sp.Point(3, 2), sp.Point(3, 3), sp.Point(2, 4), sp.Point(1, 3), sp.Point(1, 2),  # Figure 1
+        sp.Point(8, 1), sp.Point(9, 2), sp.Point(9, 3), sp.Point(8, 4), sp.Point(7, 3), sp.Point(7, 2),  # Figure 2
+        sp.Point(8, 8), sp.Point(9, 9), sp.Point(9, 10), sp.Point(8, 11), sp.Point(7, 10), sp.Point(7, 9),  # Figure 3
+        sp.Point(2, 8), sp.Point(3, 9), sp.Point(3, 10), sp.Point(2, 11), sp.Point(1, 10), sp.Point(1, 9),  # Figure 4
+    )
+
+    points_combinations = []
+
+    for start_idx in range(0, 24, 6):
+        points_combinations.extend(list(itertools.combinations(points[start_idx:start_idx + 6], 2)))
+
+    edges = [sp.Segment(first, second) for first, second in points_combinations]
+
+    edges.extend([
+        sp.Segment(points[0], points[6]), sp.Segment(points[0], points[12]), sp.Segment(points[0], points[18]),
+        sp.Segment(points[6], points[12]), sp.Segment(points[6], points[18]),
+        sp.Segment(points[12], points[18]),
+    ])
+
+    graph = sp.Graph()
+
+    for edge in edges:
+        graph.add_edge(edge)
+
+    start = timer()
+    sl.build_routes(list(points), clusters_amt, graph, processes_num)
+    execution_time = timer() - start
+
+    assert execution_time > sl._TSP_TIMELIMIT * clusters_amt / processes_num
+    assert execution_time <= (sl._TSP_TIMELIMIT + sl._ROUTING_TIMELIMIT) * clusters_amt / processes_num
